3143d78c3bf9f2f89d2d38cf4146f36541ecd0d3
Leo Irakliotis <lgreco@gmail.com>
2026-05-20 10:06:39 -0500

    pasta

--- comp271su26/pasta.py
+++ comp271su26/pasta.py
@@ -6,6 +6,7 @@ WATER_PER_GUEST: float = 0.5  # liters per additional person
 
 
 def pasta_recipe(number_of_guests: int):
+    """Compute simple amounts for pasta recipe."""
     amount_of_water: float = BASE_WATER + (number_of_guests - 1) * WATER_PER_GUEST
     amount_of_pasta: float = number_of_guests * AMOUNT_PER_PERSON
     return amount_of_water, amount_of_pasta
